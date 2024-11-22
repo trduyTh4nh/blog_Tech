1e44419c24a61a1f715d992d272c9fd174ef6a04
Duy Thanh <tttranduy999@gmail.com>
2024-11-22 20:14:42 +0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/")
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/hello")
+  sayHi() {
+    return {
+      message: "Hello world!",
+    };
   }
 }
